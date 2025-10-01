3c497a82be09dda9d61fe41e65338783c4b657b5
ThatChillProgrammer <bite74034@gmail.com>
2025-09-30 17:37:51 -0700

    commit2

--- main.py
+++ main.py
@@ -1,51 +1,59 @@
 from PIL import Image
 
-myString = "HelloWorld"
+myString = "Hello World"
 
 def textToBinary(text):
+    text += "/"
     binary = ''.join(format(ord(i), '08b') for i in text)
     return binary
 
 def encodeImage(imagePath, text):
-    img = Image.open(imagePath)
-    img = img.convert("RGB")
+    img = Image.open(imagePath).convert("RGB")
     pixels = list(img.getdata())
-
     copyList = pixels.copy()
-    """for i in range(len(text)):
-        for j in range(2):
+    current_pixel = 0
+    i = 0
+
+    while i != len(text):
+        for j in range(3):
             if int(text[i]):
-                print("1")
-                if (pixels[i][j] %  2) == 0:
-                    print("odd", i, j)
-                    copyList[i] = list(copyList[i])
-                    copyList[i][j] += 1
+                if (pixels[current_pixel][j] %  2) == 0:
+                    copyList[current_pixel] = list(copyList[current_pixel])
+                    copyList[current_pixel][j] += 1 
             else:
-                print("0")
-                if (pixels[i][j] %  2) != 0:
-                    print("even")
-                    copyList[i] = list(copyList[i])
-                    copyList[i][j] += 1"""
-    
-            
+                if (pixels[current_pixel][j] %  2) != 0:
+                    copyList[current_pixel] = list(copyList[current_pixel])
+                    copyList[current_pixel][j] += 1 
+            i+=1
+            if i==len(text):break
+        img.putpixel((current_pixel % img.width, current_pixel // img.width), tuple(copyList[current_pixel]))
+        current_pixel += 1
 
-    
-    
-    
-    
-    
-    #img.putpixel((i % img.width, i // img.width), (255, 255, 255)) 
-    #img.save("encoded_image.png")
+    img.save("encoded_image.png")
 
 
-    print(text)
-    print(pixels[:len(text)],"\n")
-    print(copyList[:len(text)])
-    """img = Image.open("./encoded_image.png")
-    img = img.convert("RGB")
-    pixels = list(img.getdata())"""
-    
-    
+def decodeImage (path) :
+    EightBitlist = []
+    message = ""
+    pixels = list(Image.open(path).convert("RGB").getdata())
+    eightBitString = ""
 
+    for i in range(len(pixels)):
+        for j in range(3):
+            if (pixels[i][j] % 2) != 0:
+                eightBitString += "1"
+            else: eightBitString += "0"
+            if len(eightBitString) == 8: 
+                EightBitlist.append(eightBitString)
+                eightBitString = ""
+
+    for i in range(len(EightBitlist)):
+        currentChar = chr(int(EightBitlist[i], 2))
+        if currentChar == "/": 
+            print(message)
+            break
+        message += currentChar
+        
 
 encodeImage("./image/sample_image.jpg",textToBinary(myString))
+decodeImage("./encoded_image.png")
